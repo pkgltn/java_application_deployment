712195c770251ed2dc919e91fd306526da6d2241
priya <a@a.com>
2023-06-30 12:31:36 +0200

    type:chore and refactor subject: Update of the maven file pom.xml with all the necessary dependencies for the project which allows it to run/execute.  Separate the project into 2 different modules: one for the ImageService and other for the Security Service. Update of the module-info files within both modules.  Create a testing class for SecurityService.

---
+++ starter/catpoint-parent/SecurityService/src/test/java/com/udacity/catpoint/service/SecurityServiceTest.java
@@ -0,0 +1,18 @@
+package com.udacity.catpoint.service;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SecurityServiceTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
